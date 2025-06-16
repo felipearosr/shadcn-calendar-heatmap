25a3942a7b417fcbba36998dfc620cabdef36bd1
Felipe Aros <felipe.aros.r@gmail.com>
2025-06-16 00:03:34 -0400

    dev error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,69 +1,8 @@
 import "./globals.css"
-import type { Metadata } from "next"
 import { cn } from "@/lib/utils"
 import { GeistSans } from "geist/font/sans"
-import { JetBrains_Mono } from "next/font/google"
 import { Toaster } from "@/components/ui/sonner"
-import { siteConfig } from "@/config/site"
 
-export const fontMono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  keywords: [
-    "React",
-    "Heatmap",
-    "Calendar",
-    "Next.js",
-    "Tailwind CSS",
-    "Server Components",
-    "Accessible",
-    "Shadcn",
-  ],
-  authors: [
-    {
-      name: "gurbaaz",
-      url: "https://gurbaaz.me",
-    },
-  ],
-  creator: "gurbaaz",
-  openGraph: {
-    type: "website",
-    locale: "en_IN",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@GurbaazNandra",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 export default function RootLayout({
   children,
